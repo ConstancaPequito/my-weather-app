29833d0e995708fd41c8f54d1547fa0196f85fe0
ConstancaPequito <96587884+ConstancaPequito@users.noreply.github.com>
2022-01-25 21:33:24 +0100

    corrected formatdate function

--- src/app.js
+++ src/app.js
@@ -26,4 +26,12 @@ function formatDate(timestamp) {
     let date = new Date(timestamp * 1000);
     let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     let day = days[date.getDay()];
+    let hours = date.getHours();
+        if (hours < 10) {
+            hours = `0${hours}`;
+        }
+    let minutes = date.getMinutes();
+        if (minutes < 10) {
+            minutes = `0${minutes}`;
+        }
 }
