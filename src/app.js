4b8f26f3c052e130954689d17e6b0ea6150dd9f4
ConstancaPequito <96587884+ConstancaPequito@users.noreply.github.com>
2022-02-28 23:32:06 +0100

    Updated formatForecastDay function

--- src/app.js
+++ src/app.js
@@ -87,7 +87,7 @@ let fahrenheintTemperature = document.querySelector("#fahrenheit-link");
 fahrenheintTemperature.addEventListener("click",displayFahrenheint);
 
 
-function formatForecastDay() {
+function formatForecastDay(timestamp) {
   let date = new Date(timestamp*1000);
   let day = date.getDay();
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
